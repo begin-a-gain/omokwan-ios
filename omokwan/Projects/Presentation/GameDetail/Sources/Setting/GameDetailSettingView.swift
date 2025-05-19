d6c6fa2ee25076d293cbe76d36b754ef5c2cb4e6
kdjun97 <kdjun97@gmail.com>
2025-05-19 23:34:04 +0900

    feat/#204: add default setting, title for setting page

--- omokwan/Projects/Presentation/GameDetail/Sources/Setting/GameDetailSettingView.swift
+++ omokwan/Projects/Presentation/GameDetail/Sources/Setting/GameDetailSettingView.swift
@@ -12,6 +12,11 @@ import DesignSystem
 public struct GameDetailSettingView: View {
     let store: StoreOf<GameDetailSettingFeature>
     @ObservedObject var viewStore: ViewStoreOf<GameDetailSettingFeature>
+    @FocusState private var focusedField: GameDetailSettingTextFieldType?
+
+    private enum GameDetailSettingTextFieldType {
+        case gameName
+    }
     
     public init(store: StoreOf<GameDetailSettingFeature>) {
         self.store = store
@@ -19,6 +24,141 @@ public struct GameDetailSettingView: View {
     }
     
     public var body: some View {
-        Text("Game Detail Setting View")
+        settingBody
     }
+    
+    private var settingBody: some View {
+        VStack(spacing: 0) {
+            ONavigationBar(
+                title: "대국 설정",
+                leadingIcon: OImages.icArrowLeft.swiftUIImage,
+                leadingIconAction: {
+                    viewStore.send(.navigateToBack)
+                }
+            )
+            scrollContent
+        }
+    }
+}
+
+private extension GameDetailSettingView {
+    var scrollContent: some View {
+        ScrollView {
+            VStack(spacing: 0) {
+                gameNameSection
+                    .padding(.bottom, 24)
+                
+                defaultSetting
+                    .padding(.bottom, 24)
+            }
+        }
+        .hPadding(20)
+        .vPadding(24)
+    }
+}
+
+// MARK: Title
+private extension GameDetailSettingView {
+    var gameNameSection: some View {
+        OTextField<GameDetailSettingTextFieldType>(
+            text: viewStore.$gameName,
+            focusedField: $focusedField,
+            focusedFieldType: .gameName,
+            label: "대국 이름",
+            isLabel: true,
+            placeholder: "대국 이름을 적어주세요."
+        )
+    }
+}
+
+// MARK: Default Settings
+private extension GameDetailSettingView {
+    var defaultSetting: some View {
+        VStack(spacing: 6) {
+            OText(
+                "기본 설정",
+                token: .subtitle_02
+            )
+            .hPadding(16)
+            .greedyWidth(.leading)
+            VStack(spacing: 0) {
+                gameDaysElapsedView
+                StrokeDivider(color: OColors.stroke02.swiftUIColor)
+                gameCodeView
+                StrokeDivider(color: OColors.stroke02.swiftUIColor)
+                repeatDayView
+                StrokeDivider(color: OColors.stroke02.swiftUIColor)
+                maxNumOfPeopleView
+            }
+            .overlay(RoundedRectangle(cornerRadius: 8).stroke(OColors.stroke02.swiftUIColor, lineWidth: 1.0))
+        }
+    }
+    
+    var gameDaysElapsedView: some View {
+        HStack(spacing: 0) {
+            OText(
+                "대국 진행 일 수",
+                token: .subtitle_03
+            )
+            Spacer()
+            OText(
+                "+123일 째",
+                token: .subtitle_03,
+                color: OColors.text02.swiftUIColor
+            )
+        }
+        .padding(16)
+    }
+    
+    var gameCodeView: some View {
+        HStack(spacing: 0) {
+            OText(
+                "대국코드",
+                token: .subtitle_03
+            )
+            Spacer()
+            Button {
+                UIPasteboard.general.string = "XALXBB6ZU2"
+            } label: {
+                OText(
+                    "XALXBB6ZU2",
+                    token: .subtitle_03,
+                    color: OColors.text02.swiftUIColor,
+                    isUnderline: true
+                )
+            }
+        }
+        .padding(16)
+    }
+    
+    var repeatDayView: some View {
+        HStack(spacing: 0) {
+            OText(
+                "반복 요일",
+                token: .subtitle_03
+            )
+            Spacer()
+            OText(
+                "월, 수, 금",
+                token: .subtitle_03,
+                color: OColors.text02.swiftUIColor
+            )
+        }
+        .padding(16)
+    }
+    
+    var maxNumOfPeopleView: some View {
+        OInputField(
+            title: "최대 인원 수",
+            value: "\(viewStore.maxNumOfPeople)",
+            buttonAction: {
+                viewStore.send(.maxNumOfPeopleButtonTapped)
+            }
+        )
+    }
+}
+
+// MARK: Other Settings
+private extension GameDetailSettingView {
+    
 }
