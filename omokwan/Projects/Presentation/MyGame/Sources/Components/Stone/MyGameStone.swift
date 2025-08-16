04adec961b4f6ab77134229027e764672cd6effa
kdjun97 <kdjun97@gmail.com>
2025-08-16 14:02:29 +0900

    feat/#267: 오목돌 UI 리팩토링 및 Component화 완료

--- omokwan/Projects/Presentation/MyGame/Sources/Components/Stone/MyGameStone.swift
+++ omokwan/Projects/Presentation/MyGame/Sources/Components/Stone/MyGameStone.swift
@@ -13,6 +13,7 @@ struct MyGameStone: View {
     let fullRectSize: CGFloat
     let stoneSize: CGFloat
     let myGameCompleteStatus: MyGameCompleteStatus
+    let omokStoneType: OmokStoneType
     
     init(
         fullRectSize: CGFloat,
@@ -22,103 +23,50 @@ struct MyGameStone: View {
         self.fullRectSize = fullRectSize
         self.stoneSize = stoneSize
         self.myGameCompleteStatus = myGameCompleteStatus
+        
+        self.omokStoneType = switch myGameCompleteStatus {
+        case .complete: .primary
+        case .inComplete, .inCompleteWithSkip: .white
+        }
     }
     
     var body: some View {
         ZStack {
-            stone
-            stoneStatusButton
+            OmokStone(
+                stoneSize: stoneSize,
+                stoneType: omokStoneType
+            )
+            stoneStatusView
         }
     }
 }
 
-// MARK: 오목돌에 관한 View
 private extension MyGameStone {
-    private var stone: some View  {
-        Circle()
-            .fill(
-                LinearGradient(
-                    stops: gradientColors,
-                    startPoint: .topLeading,
-                    endPoint: .bottomTrailing
-                )
-            )
-            .opacity(stoneOpacity)
-            .frame(width: stoneSize, height: stoneSize)
-            .shadow(
-                color: stoneShadowColor,
-                radius: 10,
-                x: 0, y: 0
-            )
-            .modifier(MyGameStoneModifier(myGameCompleteStatus: myGameCompleteStatus))
-    }
-    
-    private var gradientColors: [Gradient.Stop] {
-        switch myGameCompleteStatus {
-        case .complete:
-            return [
-                Gradient.Stop(color: OColors.oWhite.swiftUIColor, location: MyGameConstants.linearGradientStartPoint),
-                Gradient.Stop(color: OColors.uiPrimary.swiftUIColor, location: MyGameConstants.linearGradientMiddlePoint),
-                Gradient.Stop(color: OColors.uiLinearGradientEndPoint.swiftUIColor, location: MyGameConstants.linearGradientEndPoint)
-            ]
-        case .inComplete, .inCompleteWithSkip:
-            return [
-                Gradient.Stop(color: OColors.oWhite.swiftUIColor, location: MyGameConstants.linearGradientStartPoint),
-                Gradient.Stop(color: OColors.gray600.swiftUIColor, location: MyGameConstants.linearGradientMiddlePoint),
-                Gradient.Stop(color: OColors.oWhite.swiftUIColor, location: MyGameConstants.linearGradientEndPoint)
-            ]
-        }
-    }
-    
-    private var stoneOpacity: CGFloat {
-        switch myGameCompleteStatus {
-        case .complete: 0.5
-        case .inComplete, .inCompleteWithSkip: 0.2
-        }
-    }
-    
-    private var stoneShadowColor: Color {
-        switch myGameCompleteStatus {
-        case .complete:
-            OColors.uiPrimary.swiftUIColor// .opacity(0.7)
-        case .inComplete, .inCompleteWithSkip:
-            OColors.oBlack.swiftUIColor
-//            OColors.uiInCompletedStoneShadow.swiftUIColor
-        }
-    }
-}
-
-// MARK: 오목돌의 status (완료, 미완료와 같은 상태)에 대한 View
-private extension MyGameStone {
-    private var stoneStatusButton: some View {
+    var stoneStatusView: some View {
         ZStack(alignment: .topTrailing) {
             Color.clear
             
-            Button {
-                
-            } label: {
-                ZStack {
-                    Circle()
-                        .fill(stoneStatusButtonBackgroundColor)
-                        .padding(.trailing, 4)
-                        .padding(.top, 4)
-                        .frame(fullRectSize / 3, fullRectSize / 3)
-                        .modifier(
-                            MyGameStoneStatusButtonModifier(
-                                myGameCompleteStatus: myGameCompleteStatus,
-                                size: fullRectSize / 3
-                            )
+            ZStack {
+                Circle()
+                    .fill(stoneStatusButtonBackgroundColor)
+                    .padding(.trailing, 4)
+                    .padding(.top, 4)
+                    .frame(fullRectSize / 3, fullRectSize / 3)
+                    .modifier(
+                        MyGameStoneStatusViewModifier(
+                            myGameCompleteStatus: myGameCompleteStatus,
+                            size: fullRectSize / 3
                         )
-                    stoneStatusButtonContent
-                        .padding(.trailing, 4)
-                        .padding(.top, 4)
-                }
+                    )
+                stoneStatusButtonContent
+                    .padding(.trailing, 4)
+                    .padding(.top, 4)
             }
         }.frame(fullRectSize, fullRectSize)
     }
     
     @ViewBuilder
-    private var stoneStatusButtonContent: some View {
+    var stoneStatusButtonContent: some View {
         switch myGameCompleteStatus {
         case .complete:
             OImages.icCheck.swiftUIImage
@@ -139,7 +87,7 @@ private extension MyGameStone {
         }
     }
     
-    private var stoneStatusButtonBackgroundColor: Color {
+    var stoneStatusButtonBackgroundColor: Color {
         switch myGameCompleteStatus {
         case .complete:
             OColors.uiPrimary.swiftUIColor
