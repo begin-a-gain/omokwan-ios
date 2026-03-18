33c6114877d6ca27c1abfbdb715786aa0770e480
Jumy <kdjun97@gmail.com>
2026-03-18 21:05:16 +0900

    feat/#428: Lottie 사용을 위한 tuist 설정

--- omokwan/Package.swift
+++ omokwan/Package.swift
@@ -17,6 +17,7 @@ let package = Package(
         .package(url: "https://github.com/Swinject/Swinject.git", from: "2.9.1"),
         .package(url: "https://github.com/kakao/kakao-ios-sdk.git", from: "2.20.0"),
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "1.16.0"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "12.5.0")
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "12.5.0"),
+        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.6.0")
     ]
 )
---
+++ omokwan/Projects/DesignSystem/Sources/Components/Lottie/OLottie.swift
@@ -0,0 +1,9 @@
+//
+//  OLottie.swift
+//  DesignSystem
+//
+//  Created by jumy on 3/18/26.
+//  Copyright © 2026 begin-a-gain. All rights reserved.
+//
+
+import Foundation
