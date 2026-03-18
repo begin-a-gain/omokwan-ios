a6082ac4776ac287dd0b736b6a717793526824f4
Jumy <kdjun97@gmail.com>
2026-03-18 21:05:38 +0900

    feat/#428: lottie 코드 추가

--- omokwan/Projects/DesignSystem/Sources/Components/Lottie/OLottie.swift
+++ omokwan/Projects/DesignSystem/Sources/Components/Lottie/OLottie.swift
@@ -3,7 +3,32 @@
 //  DesignSystem
 //
 //  Created by jumy on 3/18/26.
-//  Copyright © 2026 begin-a-gain. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+import Lottie
+
+public struct OLottieView: View {
+    private let animation: LottieAnimation?
+    private let loopMode: LottieLoopMode
+    private let speed: CGFloat
+    
+    public init(
+        _ asset: AnimationAsset,
+        loopMode: LottieLoopMode = .loop,
+        speed: CGFloat = 1.0
+    ) {
+        self.animation = asset.animation
+        self.loopMode = loopMode
+        self.speed = speed
+    }
+    
+    public var body: some View {
+        LottieView(animation: animation)
+            .configure { animationView in
+                animationView.loopMode = loopMode
+                animationView.animationSpeed = speed
+            }
+            .playing()
+    }
+}
